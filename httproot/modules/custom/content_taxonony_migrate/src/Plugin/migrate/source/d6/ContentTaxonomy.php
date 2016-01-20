f038c407672a8204ecaf8eebd3821614150b0c04
Robert Foley <robertfoleyjr@gmail.com>
2016-01-20 14:31:22 -0800

    content taxonony base setup

---
+++ httproot/modules/custom/content_taxonony_migrate/src/Plugin/migrate/source/d6/ContentTaxonomy.php
@@ -0,0 +1,16 @@
+<?php
+/** 
+ * @file
+ * Contains \Drupal\content_taxonomy_migrate\Plugin\migrate\source\d6\contenttaxonomy
+ */
+
+namespace Drupal\content_taxonomy_migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+
+/**
+ * Source for content taxonomy fields.
+ * 
+ * @Migratesource(
+ *    id = '
+ */
