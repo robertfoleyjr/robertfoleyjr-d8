09e748c8b39afacbbb6e23e942c62636689307fa
Robert Foley <robertfoleyjr@gmail.com>
2016-01-22 14:40:41 -0800

    renaming and setting up structure

--- httproot/modules/custom/content_taxonony_migrate/src/Plugin/migrate/source/d6/ContentTaxonomy.php
+++ httproot/modules/custom/content_taxonony_migrate/src/Plugin/migrate/source/d6/ContentTaxonomy.php
@@ -1,7 +1,7 @@
 <?php
 /** 
  * @file
- * Contains \Drupal\content_taxonomy_migrate\Plugin\migrate\source\d6\contenttaxonomy
+ * Contains \Drupal\content_taxonomy_migrate\Plugin\migrate\source\d6\ContentTaxonomy
  */
 
 namespace Drupal\content_taxonomy_migrate\source;
@@ -12,5 +12,30 @@ use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
  * Source for content taxonomy fields.
  * 
  * @Migratesource(
- *    id = '
+ *    id = "ContentTaxonomy"
+ * )
  */
+class ContentTaxonomy extends SourcePluginBase {
+  
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+    
+  }
+
+  protected function initializeIterator() {
+    
+  }
+
+  public function __toString() {
+    
+  }
+
+  public function fields() {
+    
+  }
+
+  public function getIds() {
+    
+  }
+
+}
